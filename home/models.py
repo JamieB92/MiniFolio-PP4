3a3ce29d7b242678231d54a7d424c7661841a739
JamieB92 <>
2023-08-21 19:03:36 +0000

    create database model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+# DB Model for User Posts
+class userPosts(models.Model):
+    header = models.CharField(max_length=100, unique=True)
+    creator = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="creator_posts")
+    posted_on = models.DateTimeField(auto_now_add=True)
+    post_image = CloudinaryField('image')
+    caption = models.TextField(max_length=500)
+    super_vote = models.ManyToManyField(
+        User, related_name='post_super_votes', blank=True)
+    up_vote = models.ManyToManyField(
+        User, related_name='post_up_votes', blank=True)
+    down_vote = models.ManyToManyField(
+        User, related_name='post_down_votes', blank=True)
+
+
+def __str__(self):
+    return self.header
+
+
+# Counts for Super, Up and Down Votes
+def total_up_votes(self):
+    return self.up_vote.count
+
+
+def total_super_vote(self):
+    return self.super_vote.count
+
+
+def total_down_vote(self):
+    return self.down_vote.count
+
+# DB Model for posts comments
+
+
+class postComments(models.Model):
+    post_comments = models.ForeignKey(userPosts, on_delete=models.CASCADE,
+                                      related_name="post_comments")
+    name = models.CharField(max_length=80)
+    comment_body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.comment_body} by {self.name}"
--- userprofile/models.py
+++ userprofile/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from home.models import userPosts
 
-# Create your models here.
+
+class userProfiles(models.Model):
+
+    profile_image = CloudinaryField('image')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=False,
+                             blank=False, related_name='user_profile')
+    username = models.CharField(max_length=30, blank=True, null=True)
+    profile_bio = models.TextField(max_length=350, blank=True)
+
+
+def __str__(self):
+    return str(self.user)
+
+
+def users_post(self):
+    return self.creator.all()
