d82c8437184c258e20612518a2cbed42e43bb4a1
JamieB92 <>
2023-08-22 22:14:26 +0000

    create initial base.html and index.html

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import userPosts
 
-# Create your views here.
+
+class UploadList(generic.ListView):
+    model = userPosts
+    queryset = userPosts.objects.order_by("-posted_on")
+    template_name = "index.html"
