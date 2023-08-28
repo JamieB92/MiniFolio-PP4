68c1dc41e8d4c888db5a990356e704b579945b6f
JamieB92 <>
2023-08-28 20:20:25 +0000

    create initial home page with MiniFolio styling

--- home/views.py
+++ home/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import userPosts
+from .models import postComments
 
 
 class LandingPage(View):
     def get(self, request):
-       return render(request, "index.html")
+        return render(request, "index.html")
 
 
 class UploadList(generic.ListView):
-    model = userPosts
+    model = userPosts, postComments
     queryset = userPosts.objects.order_by("-posted_on")
     template_name = "home.html"
