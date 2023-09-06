ef659b1ca82cc582b8f87da7f74c4a3480634e58
JamieB92 <>
2023-09-06 19:20:33 +0000

    add a create profile page

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
 from .views import UserProfile
+from .views import CreateUserProfile
 
 
 urlpatterns = [
     path('profile', UserProfile.as_view(), name="profile"),
+    path('create-profile', CreateUserProfile.as_view(), name="create-profile"),
 ]
--- userprofile/views.py
+++ userprofile/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
 from .models import userPosts
 from .models import userProfiles
@@ -9,3 +10,6 @@ class UserProfile(ListView):
     model = userProfiles
     template_name = "profile.html"
 
+
+class CreateUserProfile(CreateView):
+   fields = ['Profile_image', 'profile_bio']
