5a45d1469416fd9aff2fb103731d249695ef4bdb
JamieB92 <>
2023-09-10 21:48:11 +0000

    remove whitenoise install that broke heroku deployment

--- minifolio/settings.py
+++ minifolio/settings.py
@@ -81,7 +81,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'minifolio.urls'
