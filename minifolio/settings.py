5f56db0588a86f3e77ef5dbc3fcdbd0ae64d7d0a
JamieB92 <>
2023-09-10 21:16:09 +0000

    Deployment commit

--- minifolio/settings.py
+++ minifolio/settings.py
@@ -30,7 +30,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-jamieb92-minifolio-pp4-u1uugib5fn.us2.codeanyapp.com',
                  '8001-jamieb92-minifolio-pp4-u1uugib5fn.us2.codeanyapp.com',
