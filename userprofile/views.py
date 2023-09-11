870701f314f73c2de11c28cb6c417380db851c52
JamieB92 <>
2023-09-11 20:03:56 +0000

    revert back for button issue

--- userprofile/views.py
+++ userprofile/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView
 from django.views.generic.edit import UpdateView
-from django.db.models import Count, Q
 from .models import userPosts
 from .models import userProfiles
 from .forms import ProfileForm
@@ -13,22 +12,14 @@ class UserProfile(ListView):
 
     def get(self, request, pk):
         profile = userProfiles.objects.get(user=pk)
-        myposts = userPosts.objects.annotate(
-            upvoted_by_user=Count
-            ('up_vote', filter=Q(up_vote=request.user)),
-            downvoted_by_user=Count
-            ('down_vote', filter=Q(down_vote=request.user)),
-            supervoted_by_user=Count
-            ('super_vote', filter=Q(super_vote=request.user))
-            ).order_by('-posted_on')
-
+        myposts = userPosts.objects.order_by("-posted_on")
         return render(
-            request, "profile.html",
+            request, 'profile.html',
             {
-                "profile": profile,
-                "myposts": myposts,
+                'profile': profile,
+                'myposts': myposts,
             }
-        )
+            )
 
 
 class CreateUserProfile(CreateView):
